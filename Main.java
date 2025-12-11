67ed840f93b10b3dd05982f7f3b9592cef062695
Shivamirji36 <shivmirji36@icloud.com>
2025-12-11 20:24:06 +0530

    Final Changes has been done

--- Main.java
+++ Main.java
@@ -1,15 +1,139 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    
-        /*
-        Warrior myObj = new Warrior("Warrior");
-        myObj.showStats();
-        System.out.println(myObj.attack());
-        myObj.takeDamage(50);
-        myObj.showStats();
-        System.out.println(myObj.isAlive());
-        */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Character: ");
+        String name = input.nextLine();
+        String winner = "";
+
+        if (name.equalsIgnoreCase("warrior")) {
+            Warrior hero = new Warrior("Warrior");
+            Enemy enemy = new Enemy("Goblin");
+
+            hero.showStats();
+            enemy.showStats();
+
+            while (hero.isAlive() && enemy.isAlive()) {
+                int damage = hero.attack();
+                enemy.takeDamage(damage);
+
+                System.out.println("Warrior deals " + hero.attack() + " damage to Enemy");
+                System.out.println("Enemy Health: " + enemy.health);
+
+                damage = enemy.attack();
+                hero.takeDamage(damage);
+
+                System.out.println("Enemy deals " + enemy.attack() + " damage to Hero");
+                System.out.println("Hero Health: " + hero.health);
+
+                if (hero.health <= 0 && enemy.health <= 0) {
+                    winner = "Double KO";
+                    break;
+                }
+                else if (hero.health <= 0) {
+                    winner = "Enemy";
+                    break;
+                } else if (enemy.health <= 0) {
+                    winner = "Hero";
+                    break;
+                }
+
+            }
+            if (winner.equals("Enemy")) {
+                System.out.println("Enemy Wins!!");
+            }
+            else if (winner.equals("Hero")) {
+                System.out.println("Warrior Win!!");
+            }
+            else {
+                System.out.println("Double KO");
+            }
+            
+        }
+        else if (name.equalsIgnoreCase("mage")) {
+            Mage hero = new Mage("Mage");
+            Enemy enemy = new Enemy("Goblin");
+
+            hero.showStats();
+            enemy.showStats();
+
+            while (hero.isAlive() && enemy.isAlive()) {
+                int damage = hero.attack();
+                enemy.takeDamage(damage);
+
+                System.out.println("Mage deals " + hero.attack() + " damage to Enemy");
+                System.out.println("Enemy Health: " + enemy.health);
+
+                damage = enemy.attack();
+                hero.takeDamage(damage);
+
+                System.out.println("Enemy deals " + enemy.attack() + " damage to Hero");
+                System.out.println("Hero Health: " + hero.health);
+
+                if (hero.health <= 0 && enemy.health <= 0) {
+                    winner = "Double KO";
+                    break;
+                } else if (enemy.health <= 0) {
+                    winner = "Hero";
+                    break;
+                } else if (hero.health <= 0) {
+                    winner = "Enemy";
+                    break;
+                }
+            }
+            if (winner.equals("Hero")) {
+                System.out.println("Mage Win!!");
+            }
+            else if (winner.equals("Enemy")) {
+                System.out.println("Enemy Win!!");
+            }
+            else {
+                System.out.println("Double KO");
+            }
+
+        }
+        else if (name.equalsIgnoreCase("archer")) {
+            Archer hero = new Archer("Archer");
+            Enemy enemy = new Enemy("Goblin");
+
+            hero.showStats();
+            enemy.showStats();
+
+            while (hero.isAlive() && enemy.isAlive()) {
+                int damage = hero.attack();
+                enemy.takeDamage(damage);
+
+                System.out.println("Archer deals " + hero.attack() + " damage to Enemy");
+                System.out.println("Enemy Health: " + enemy.health);
+
+                damage = enemy.attack();
+                hero.takeDamage(damage);
+
+                System.out.println("Enemy deals " + enemy.attack() + " damage to Hero");
+                System.out.println("Hero Health: " + hero.health);
+
+                if (hero.health <= 0 && enemy.health <= 0) {
+                    winner = "Double KO";
+                    break;
+                } else if (enemy.health <= 0) {
+                    winner = "Hero";
+                    break;
+                } else if (hero.health <= 0) {
+                    winner = "Enemy";
+                    break;
+                }
+            }
+            if (winner.equals("Hero")) {
+                System.out.println("Archer Win!!");
+            } else if (winner.equals("Enemy")) {
+                System.out.println("Enemy Win!!");
+            } else {
+                System.out.println("Double KO");
+            }
+
+        }
   }
 }
 
@@ -26,6 +150,7 @@ abstract class Character {
 
     abstract int attack();
 
+    //This method takes the damage from enemy or hero
     public void takeDamage(int damage) {
         health = health - damage;
         if (health < 0) {
@@ -33,6 +158,7 @@ abstract class Character {
         }
     }
 
+    //This method checks whether the Hero or Enemy is alive
     public boolean isAlive(){
         if(health > 0){
             return true;
@@ -42,6 +168,7 @@ abstract class Character {
         }
     }
 
+    //This method shows the Stats of Enemy or Hero
     public void showStats(){
         System.out.println("Character: " + name);
         System.out.println("Health: " + health);

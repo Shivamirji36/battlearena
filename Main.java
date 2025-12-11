09e7545ff40165436d603760b82497c74fa580af
Shivamirji36 <shivmirji36@icloud.com>
2025-12-11 17:57:59 +0530

    Class character with variables created

--- Main.java
+++ Main.java
@@ -1,6 +1,40 @@
 public class Main {
 
-  public static void main(Static[] args) {}
+  public static void main(String[] args) {}
 }
 
-abstract class Character {}
+abstract class Character {
+    protected String name;
+    protected int health;
+    protected int attackPower;
+
+    Character(String name, int health, int attackPower){
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    abstract int attack();
+
+    public void takeDamage(int damage) {
+        health = health - damage;
+        if (health < 0) {
+            health = 0;
+        }
+    }
+
+    public boolean isAlive(){
+        if(health > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void showStats(){
+        System.out.println("Character: " + name);
+        System.out.println("Health: " + health);
+        System.out.println("Attack power: " + attackPower);
+    }
+}
